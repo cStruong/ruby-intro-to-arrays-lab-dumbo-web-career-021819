128d6602d9dc496f07cb2e5ed86a50917f2cdcee
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-26 02:09:59 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,39 @@
+def instantiate_new_array
+  arr = []
+  return arr
+end
+
+def array_with_two_elements
+  secArr = ['Red', 'Velvet']
+  return secArr
+end
+
+def first_element(array)
+  firstIdx = array[0]
+  return firstIdx
+end
+
+def third_element(array)
+  thirdIdx = array[2]
+  return thirdIdx
+end
+
+def last_element(array)
+  lastIdx = array[-1]
+  return lastIdx
+end
+
+def first_element_with_array_methods(array)
+  firstEle = array.first
+  return firstEle
+end
+
+def last_element_with_array_methods(array)
+  lastEle = array.last
+  return lastEle
+end
+
+def length_of_array(array)
+  length = array.length
+  return length
+end
